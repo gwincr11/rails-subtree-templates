a22220f4dc41f8a5b824d23d3020d3fdd19057f7
Cory Gwin @gwincr11 <gwincr11@github.com>
2017-07-06 19:20:01 -0400

    per domain sso credentials

--- config/initializers/omniauth-githubber.rb
+++ config/initializers/omniauth-githubber.rb
@@ -1,6 +1,17 @@
+require 'yaml'
+
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :githubber, '383954ae4598f7825afe', 'f47d16c996af407b884a8b899d0bc5d0ac9d1242', {
-    :scope => 'user'
-  }
+
+SETUP_PROC = lambda do |env|
+  request = Rack::Request.new(env)
+  config_file =  YAML.load_file("domains.yml")
+
+  configs = config_file["domains"][request.env["SERVER_NAME"]]
+  env['omniauth.strategy'].options[:client_id] =  configs["sso_client_id"]
+  env['omniauth.strategy'].options[:client_secret] =  configs["sso_client_secret"]
+  env['omniauth.strategy'].options[:scope] = 'user'
+end
+
+  provider :githubber, setup: SETUP_PROC
 end
 OmniAuth.config.logger = Rails.logger
